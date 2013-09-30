1607dff14131ccbe8d96370cb1b333244c03cd8d
Graham Tremper <gtremper@gmail.com>
2013-09-30 12:23:07 -0700

    added scafolding for app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('FoodTruckApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
